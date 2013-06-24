62433cac47fc716b24749d49760dd7fc1d8e71e2
Kevin Burke <kevin@twilio.com>
2013-06-23 17:01:08 -0700

    add correct page size

--- crawler/migration.py
+++ crawler/migration.py
@@ -7,8 +7,9 @@ import sqlalchemy
 count = 0
 session = db.get_session()
 start = time.time()
+PAGE_SIZE = 1000
 while True:
-    users = session.query(User).order_by(User.id).limit(10).offset(count * 1000)
+    users = session.query(User).order_by(User.id).limit(PAGE_SIZE).offset(count * PAGE_SIZE)
     for user in users:
         try:
             db.add_indexed_user(session, user)
@@ -17,4 +18,4 @@ while True:
             with open('duplicates.csv', 'a') as f:
                 f.write('{},{}\n'.format(user.username, user.id))
     count += 1
-    print "{} users crawled, {} seconds elapsed".format(count * 1000, int(time.time() - start))
+    print "{} users crawled, {} seconds elapsed".format(count * PAGE_SIZE, int(time.time() - start))
